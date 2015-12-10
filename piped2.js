2771b4034dd654f07b7b46f2a97368fe8b1321c5
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-12-10 02:07:43 -0500

    piped2 produces sequences

--- piped2.js
+++ piped2.js
@@ -6,7 +6,12 @@ ncbi.search('protein', 'mbp1')
     .pipe(filter.obj(function (obj) {
         return obj.title.match(/^Mbp1p \[Saccharomyces cerevisiae [^Y]/);
     }))
-    .pipe(es.through(function (data) {
-        this.emit('data', data.title + '\n');
+    .pipe(es.through(function (obj) {
+        this.emit('data', obj.gi + '\n');
+    }))
+    .pipe(ncbi.fetch('protein'))
+    .pipe(es.stringify())
+    .pipe(es.through(function (str) {
+        this.emit('data', str + '\n');
     }))
     .pipe(process.stdout);
