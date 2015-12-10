11869195005a45af4d6e48af9d1e907c194c3214
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-12-10 02:25:13 -0500

    fix piped2 for nicer ouput

--- piped2.js
+++ piped2.js
@@ -2,8 +2,6 @@ var ncbi = require('bionode-ncbi');
 var es = require('event-stream');
 var filter = require('through2-filter');
 
-console.log(Object.keys(dat));
-
 ncbi.search('protein', 'mbp1')
     .pipe(filter.obj(function (obj) {
         return obj.title.match(/^Mbp1p \[Saccharomyces cerevisiae [^Y]/);
@@ -14,6 +12,6 @@ ncbi.search('protein', 'mbp1')
     .pipe(ncbi.fetch('protein'))
     .pipe(es.stringify())
     .pipe(es.through(function (str) {
-        this.emit('data', str + '\n');
+        this.emit('data', str);
     }))
     .pipe(process.stdout);
